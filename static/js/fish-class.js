481f4945c70df65b155925ad377725c07af0a215
ststaynov <ststaynov@hotmail.com>
2016-12-03 19:09:17 +0100

    attempt to get ball values from websocket data

--- static/js/fish-class.js
+++ static/js/fish-class.js
@@ -1,10 +1,12 @@
 /**
  * Created by ststa on 12/3/2016.
  */
-function Fish(data) {
 
-    this.player = data.handle;
-    this.position = data.message;
+function init(){
+
+
+    //this.player = data.handle;
+    //this.position = data.message;
     //
     //console.log(data);
     //this.getInfo = function () {
@@ -20,8 +22,8 @@ function Fish(data) {
     var vy = 0;
 
     // Acceleration
-    var ax = 0;
-    var ay = 0;
+    this.ax = 0;
+    this.ay = 0;
 
     var delay = 10;
     var vMultiplier = 0.01;
@@ -29,38 +31,47 @@ function Fish(data) {
 //TODO make ax and ay get the value from position(data.message)
 //var streetaddress= addy.substr(0, addy.indexOf(','));
 //    window.ondevicemotion = function (event) {
-        ax = this.position.substr(0, this.position.indexOf('y'));
-        ay = this.position.substr(this.position.indexOf("y") + 1);
-        //console.log("Accelerometer data - x: " + event.accelerationIncludingGravity.x + " y: " + event.accelerationIncludingGravity.y + " z: " + event.accelerationIncludingGravity.z);
+//        ax = this.position.substr(0, this.position.indexOf('y'));
+//        ay = this.position.substr(this.position.indexOf("y") + 1);
+    //console.log("Accelerometer data - x: " + event.accelerationIncludingGravity.x + " y: " + event.accelerationIncludingGravity.y + " z: " + event.accelerationIncludingGravity.z);
 
 
-        setInterval(function () {
-            vy = vy + -(ay);
-            vx = vx + ax;
+    setInterval(function () {
+        vy = vy + -(init.ay);
+        vx = vx + init.ax;
 
-            var ball = document.getElementById("ball");
-            y = parseInt(y + vy * vMultiplier);
-            x = parseInt(x + vx * vMultiplier);
+        var ball = document.getElementById("ball");
+        y = parseInt(y + vy * vMultiplier);
+        x = parseInt(x + vx * vMultiplier);
 
-            if (x < 0) {
-                x = 0;
-                vx = 0;
-            }
-            if (y < 0) {
-                y = 0;
-                vy = 0;
-            }
-            if (x > window.innerWidth - 20) {
-                x = window.innerWidth - 20;
-                vx = 0;
-            }
-            if (y > window.innerHeight - 20) {
-                y = window.innerHeight - 20;
-                vy = 0;
-            }
+        if (x < 0) {
+            x = 0;
+            vx = 0;
+        }
+        if (y < 0) {
+            y = 0;
+            vy = 0;
+        }
+        if (x > window.innerWidth - 20) {
+            x = window.innerWidth - 20;
+            vx = 0;
+        }
+        if (y > window.innerHeight - 20) {
+            y = window.innerHeight - 20;
+            vy = 0;
+        }
 
-            ball.style.top = y + "px";
-            ball.style.left = x + "px";
-        }, delay);
-    //}
+        ball.style.top = y + "px";
+        ball.style.left = x + "px";
+    }, delay);
 }
+
+
+function moveFish(data) {
+    this.position = data.message;
+
+    init.ax = this.position.substr(0, this.position.indexOf('y'));
+    init.ay = this.position.substr(this.position.indexOf("y") + 1);
+}
+
+init();
